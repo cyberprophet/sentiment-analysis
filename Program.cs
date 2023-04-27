52678774e0d7596dc3103e10162ae32b15b2b575
SANGWOO PARK <cyberprophet@hanmail.net>
2023-04-27 22:02:04 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SentimentAnalysis;
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+    string _dataPath = Path.Combine(
+        Directory.GetParent(
+        Directory.GetParent(        Environment.CurrentDirectory)!.FullName)!.FullName
+        , "Data", "yelp_labelled.text");
+}
